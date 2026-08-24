4cd534d42aafda25a2bc3fdc090f4c2bfca76749
Lavish Bana <lavishbana50@gmail.com>
2026-08-24 09:58:29 +0530

    second commit

---
+++ commonjs.js
@@ -0,0 +1,6 @@
+const calculate_area = require('./main')
+console.log(calculate_area(10));
+const calculate_perimeter = require('./main')
+console.log(calculate_perimeter());
+const isVote = require('./esm')
+console.log(isVote(20));
